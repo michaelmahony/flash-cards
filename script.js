461a7ea90e44fbe5cab4ee8c7f033c441ac7183a
michaelmahony <michael.mahony@umbc.edu>
2016-07-13 14:09:06 -0400

    Core works. About to add study and test modes.

--- script.js
+++ script.js
@@ -1,73 +1,55 @@
-function showText(div ,text, label) {
-    $(div + " .card-text").html(text);
-    $(div + " .front-back-label").html(label);
+function showText(text, label) {
+    $(".card-text").html(text);
+    $(".front-back-label").html(label);
+}
+
+function flipCard() {
+    if ($(".flip").val() == 'Show Back') {
+        $(".flip").val('Show Front');
+        showText(theCard.back, 'Back');
+        // If first time being clicked, show the grading buttons
+        if ($(".grade-btns").css('visibility') == 'hidden') {
+            $(".grade-btns").css('visibility', 'visible');
+        }
+
+    } else if ($(".flip").val() == 'Show Front') {
+        showText(theCard.front, 'Front');
+        $(".flip").val('Show Back');
+    }
 }
 
 function serveCard (theCard) {
-
-
     // Show the front text
-    showText(".front", theCard.front, 'Front');
-    showText(".back", theCard.back, 'Back');
-
-
-    var state = 'front';
-
-    $(".flip").on("click", function () {
-        if ($(".flip").val() == 'Show Back') {
-            $(".flip").val('Show Front');
-            // If first time being clicked, show the grading buttons
-            if ($(".grade-btns").css('visibility') == 'hidden') {
-                $(".grade-btns").css('visibility', 'visible');
-            }
-
-        } else if ($(".flip").val() == 'Show Front') {
-            // showText(theCard.front, 'Front');
-            $(".flip").val('Show Back');
-        }
-    });
-
+    showText(theCard.front, 'Front');
+    $(".flip").val('Show Back');
 }
 
 function showNextCard(counter, cardObjArray) {
-    counter++;
+
+    console.log("-----------------------------------------------------")
+    console.log('Counter: ' + counter);
+    console.log('Console mod: ' + counter % cardObjArray.length)
 
     $('.card').removeClass('flipped');
+    $('.grade-btns').css('visibility', 'hidden');
 
-    theCard = cardObjArray[cardObjArray.length % counter];
+    theCard = cardObjArray[counter % cardObjArray.length];
+    console.log(theCard.front);
 
     serveCard(theCard);
-
-
-    console.log(theCard.front);
-    console.log(theCard.back);
-    console.log(theCard.history);
-
-
-
-
-    $(".incorrect").on("click", function () {
-        theCard.setHistory(0);
-        console.log(theCard.history);
-        showNextCard(counter, cardObjArray);
-
-    })
-
-    // $(".shakey").on("click", function () {
-    //     theCard.setHistory(0);
-    //     console.log(theCard.history);
-    //
-    // })
-    // $(".correct").on("click", function () {
-    //     theCard.setHistory(1);
-    //     console.log(theCard.history);
-    //
-    // })
 }
 
-$('document').ready(function () {
-    console.log("JS Loaded");
+function test(cardObjSet) {
 
+}
+
+function study(cardObjSet) {
+
+}
+
+
+
+$('document').ready(function () {
     // The card set
     var cardSet = {
         setTitle: "Ecce Romani Chapter 1",
@@ -122,7 +104,10 @@ $('document').ready(function () {
         ],
     };
 
-    // Put each card into a Card object, in another list
+    // Set the page header
+    $('.setTitle').html(cardSet['setTitle'])
+
+    // Put each card into a Card object in another list
     cardObjArray = [];
     for (i=0; i<cardSet.cards.length; i++) {
         var cardInfo = cardSet.cards[i];
@@ -130,12 +115,46 @@ $('document').ready(function () {
         cardObjArray.push(cardObj);
     }
 
-    console.log(cardObjArray);
 
+    // Create the counter to be used to serve the rest of the cards
+    var counter = 0;
+
+    // And the subtitle / counter
+    $('.subtitle').html('Test mode: Card ' + (counter + 1) + " of " + cardObjArray.length);
+
+
+    // Serve the first card
     showNextCard(0, cardObjArray);
+    counter++;
 
-    $(".flip").on("click", function () {
-        $('.card').toggleClass('flipped');
+
+
+
+    $(".incorrect").on("click", function () {
+
+        theCard.setHistory(0);
+        console.log(theCard.history);
+        showNextCard(counter, cardObjArray);
+        counter++;
     })
 
+    $(".shakey").on("click", function () {
+
+        theCard.setHistory(0);
+        console.log(theCard.history);
+        showNextCard(counter, cardObjArray);
+        counter++;
+    })
+
+    $(".correct").on("click", function () {
+
+        theCard.setHistory(1);
+        console.log(theCard.history);
+        showNextCard(counter, cardObjArray);
+        counter++;
+    })
+
+    $(".flip").on("click", function () {
+        flipCard();
+    })
 })
